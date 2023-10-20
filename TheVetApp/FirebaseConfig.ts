5eb2c97a9640f783ba13e7fb260ce20037e5e7cb
Juan Zambrano <jpablozambrano@hotmail.com>
2023-10-20 16:33:52 -0600

    Update FirebaseConfig.ts

--- TheVetApp/FirebaseConfig.ts
+++ TheVetApp/FirebaseConfig.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 const analytics = getAnalytics(FIREBASE_APP);
