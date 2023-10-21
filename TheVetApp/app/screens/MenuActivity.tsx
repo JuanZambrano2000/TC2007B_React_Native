d9c93b21f7403e67d07d9a8feaaa26df2f151d7d
Juan Zambrano <jpablozambrano@hotmail.com>
2023-10-20 18:20:39 -0600

    Update MenuActivity.tsx

--- TheVetApp/app/screens/MenuActivity.tsx
+++ TheVetApp/app/screens/MenuActivity.tsx
@@ -1,1 +1,12 @@
 import { View, Text } from "react-native";
+import React from "react";
+
+const MenuActivity = () => {
+    return (
+        <View>
+            <Text>Menu</Text>
+        </View>
+    )
+}
+
+export default MenuActivity;
