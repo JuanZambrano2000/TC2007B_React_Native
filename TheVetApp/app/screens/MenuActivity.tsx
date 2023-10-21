1745b5f014b792ecde0774b0cb1d5550d4a96693
Juan Zambrano <jpablozambrano@hotmail.com>
2023-10-20 20:12:31 -0600

    Update MenuActivity.tsx
    It grabs the data from firebase

--- TheVetApp/app/screens/MenuActivity.tsx
+++ TheVetApp/app/screens/MenuActivity.tsx
@@ -1,18 +1,50 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import { View, Text, Button, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
 import { NavigationProp } from "@react-navigation/native";
-import { FIREBASE_AUTH } from "../../FirebaseConfig";
-import { FIREBASE_DB } from "../../FirebaseConfig";
+import { FIREBASE_AUTH, FIREBASE_DB } from "../../FirebaseConfig";
+import { collection, onSnapshot, query, doc, setDoc } from "firebase/firestore";
 
 interface RouterProps {
     navigation: NavigationProp<any, any>;
 }
+
 const MenuActivity = ({navigation}: RouterProps) => {
+    const [animals, setAnimals] = useState<{ id: string; name: string; age: number; weight: number; }[]>([]);
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(query(collection(FIREBASE_DB, "animals")), (snapshot) => {
+            const newAnimals = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                name: doc.data().name,
+                age: doc.data().age,
+                weight: doc.data().weight
+            }));
+    
+            setAnimals(newAnimals);
+        });
+    
+        return () => unsubscribe();
+    }, []);    
+
+    const handleDetail = async (animal: { id: string; name: string; age: number; weight: number; }) => {
+        await setDoc(doc(FIREBASE_DB, "currentAnimal"), animal);
+        navigation.navigate('Detail');
+    };
+
     return (
         <View>
-           <Button title="Add new animal" onPress={() => navigation.navigate('Detail')}/> 
-           <Button title="Sign out" onPress={() => FIREBASE_AUTH.signOut()}/>
-
+            <FlatList
+                data={animals}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => (
+                    <View>
+                        <Text>{item.name}</Text>
+                        <Button title="Check Details" onPress={() => handleDetail(item)} />
+                    </View>
+                )}
+            />
+            <Button title="Add new animal" onPress={() => navigation.navigate('Detail')} /> 
+            <Button title="Sign out" onPress={() => FIREBASE_AUTH.signOut()} />
         </View>
     );
 };
